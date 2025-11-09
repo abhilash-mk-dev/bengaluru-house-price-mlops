15d8ed2c6882ed2ae0713e2a85add706bd10aa7e
abhilash-mk-dev <abhi.secondacc@gmail.com>
2025-11-09 13:39:20 +0000

    initial setup for serving API

---
+++ serving/app.py
@@ -0,0 +1,24 @@
+import mlflow
+from fastapi import FastAPI
+import pandas as pd
+
+app = FastAPI()
+
+# CONNECT MLflow
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+# LOAD MODEL FROM REGISTRY (STAGING)
+MODEL_NAME = "bengaluru_house_price_model"
+model = mlflow.pyfunc.load_model(f"models:/{MODEL_NAME}/Staging")
+
+
+@app.get("/")
+def root():
+    return {"status": "model API is running"}
+
+
+@app.post("/predict")
+def predict(data: dict):
+    df = pd.DataFrame([data])
+    pred = model.predict(df)[0]
+    return {"prediction": float(pred)}
