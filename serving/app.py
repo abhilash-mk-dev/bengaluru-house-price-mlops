4ed77c77e7c2c4def29d30d78a5a561572d57a52
abhilash-mk-dev <abhi.secondacc@gmail.com>
2025-11-10 08:15:59 +0000

    revert to local artifact loading

--- serving/app.py
+++ serving/app.py
@@ -1,51 +1,27 @@
-# import mlflow
-# from fastapi import FastAPI
-# import pandas as pd
-
-# app = FastAPI()
-
-# # MLflow tracking to ngrok server
-# mlflow.set_tracking_uri("https://enhancive-arrangeable-alvin.ngrok-free.dev")
-
-# # always load staging model
-# MODEL_NAME = "bengaluru_house_price_model"
-# model = mlflow.pyfunc.load_model(f"models:/{MODEL_NAME}/Staging")
-
-# # MODEL_PATH = "./model_artifact"
-# # model = mlflow.pyfunc.load_model(MODEL_PATH)
-
-# @app.get("/")
-# def root():
-#     return {"status": "model API is running"}
-
-# @app.post("/predict")
-# def predict(data: dict):
-#     df = pd.DataFrame([data])
-#     pred = model.predict(df)[0]
-#     return {"prediction": float(pred)}
-
 import mlflow
 from fastapi import FastAPI
 import pandas as pd
 
 app = FastAPI()
 
-# PUBLIC NGROK MLflow tracking URI
-MLFLOW_NGROK = "https://enhancive-arrangeable-alvin.ngrok-free.dev"
-mlflow.set_tracking_uri(MLFLOW_NGROK)
+# CONNECT MLflow
+mlflow.set_tracking_uri("http://172.17.0.1:5000")
 
-# LOAD MODEL FROM REGISTRY (Staging stage)
-MODEL_NAME = "bengaluru_house_price_model"
-model = mlflow.pyfunc.load_model(f"models:/{MODEL_NAME}/Staging")
+# LOAD MODEL FROM REGISTRY (STAGING)
+# MODEL_NAME = "bengaluru_house_price_model"
+# model = mlflow.pyfunc.load_model(f"models:/{MODEL_NAME}/Staging")
+
+MODEL_PATH = "./model_artifact"
+model = mlflow.pyfunc.load_model(MODEL_PATH)
+
 
 @app.get("/")
 def root():
     return {"status": "model API is running"}
+
 
 @app.post("/predict")
 def predict(data: dict):
     df = pd.DataFrame([data])
     pred = model.predict(df)[0]
     return {"prediction": float(pred)}
-
-
